a1a08ffc48efd0015e4315d5cd75265f49b4fb1b
Antony <antonydellavecchia@gmail.com>
2022-01-21 13:50:29 +0100

    printing hulls

--- polygen_from_pdb.pl
+++ polygen_from_pdb.pl
@@ -13,6 +13,52 @@ my $atomdata = load($dirname . "/" . $name . "-map.txt");
 
 my $limit = 1000;
 my @atom_coords;
+
+sub seperate_amino {
+  my $atom_seq = $_[0];
+  my $new_atom_coord = new Vector<Rational>();
+  my $amino_acid = "";
+  my @atom_vertices;
+  my @aminos = [];
+  
+  while (my ($k,$v)=each %$atom_seq) {
+    if ($k == 0) {
+      next;
+    }
+    
+    if ($amino_acid ne $v->[1]) {
+      $amino_acid = $v->[1];
+
+      # finish with previous sequence
+      if (@$new_atom_coord){
+	print $new_atom_coord, "\n";
+	push @atom_vertices, $new_atom_coord;
+
+	my $atom_seq_pm = new Array<Vector<Rational>>(\@atom_vertices);
+
+	print $atom_seq_pm, "\n";
+	my $amino_hull = new Polytope<Rational>(POINTS=>$atom_seq_pm);
+	#print $amino_hull;
+	push @aminos, $amino_hull;
+      }
+      # start new amino sequence
+      @atom_vertices = ();
+      $new_atom_coord = new Vector<Rational>([1,$v->[4],$v->[5],$v->[6]]);
+      push @atom_vertices, $new_atom_coord;
+    } else {
+      $new_atom_coord = new Vector<Rational>([1,$v->[4],$v->[5],$v->[6]]);
+      push @atom_vertices, $new_atom_coord;
+	
+    }
+  }
+
+  return @aminos;
+}
+
+my $amino_hulls = seperate_amino($atomdata);
+print $amino_hulls;
+
+
 while (my ($k,$v)=each %$atomdata) {
    if ($k == 0){
       next;
